61a42084ae0e356f4a18c4680f952ba8cd2e4d16
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-17 19:55:32 -0600

    Atention

--- src/Containers/Games.js
+++ src/Containers/Games.js
@@ -3,9 +3,10 @@ import CardGame from '../components/CardGame';
 import axios from 'axios';
 
 const Games = () => {
-
     return (
-       
+        <div>
+            
+        </div>
     )
 }
 export default Games;
