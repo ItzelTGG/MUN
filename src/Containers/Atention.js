c1129be18edafb190e83c144682fa29af58a0a8f
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-17 19:04:27 -0600

    Borre games

--- src/Containers/Atention.js
+++ src/Containers/Atention.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import CardGame from '../components/CardGame';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 
 
 const Atention = () => {
+
+    const [cardGame, setCardGame] = useState({})
+    const ENDPOINT = 'https://mun-final-ae99f.firebaseio.com/game.json';
+
+    const getCardGame = () => {
+        axios.get(ENDPOINT)
+            .then((body) => setCardGame(body.data))
+            .catch((error) => alert(error));
+    }
+
+    useEffect(() => {
+        getCardGame();
+    }, []);
+
     return (
-        <h1>Atention</h1>
-       
+        <div className="bg-light">
+            <div className="container">
+            <h1>Atention</h1>
+                {Object.keys(cardGame).map((id) =>
+                    <CardGame
+                        key={cardGame[id].title}
+                        id={id}
+                        title={cardGame[id].title}
+                        description={cardGame[id].description}
+                        instructions={cardGame[id].instructions}
+                        materials={cardGame[id].materials}
+                        getCardGame={getCardGame} />)}
+            </div>
+        </div>
     )
+
 }
 
 export default Atention
--- src/Containers/Games.js
+++ src/Containers/Games.js
@@ -3,34 +3,9 @@ import CardGame from '../components/CardGame';
 import axios from 'axios';
 
 const Games = () => {
-    const [cardGame, setCardGame] = useState({})
-    const ENDPOINT = '';
-
-    const getCardGame = () => {
-        axios.get(ENDPOINT)
-            .then((body) => setCardGame(body.data))
-            .catch((error) => alert(error));
-    }
-
-    useEffect(() => {
-        getCardGame();
-    }, []);
 
     return (
-        <div className="bg-light">
-            <div className="container">
-                <h1>Juegos</h1>
-                {Object.keys(cardGame).map((id) =>
-                    <CardGame
-                        key={cardGame[id].title}
-                        id={id}
-                        title={cardGame[id].title}
-                        description={cardGame[id].description}
-                        instructions={cardGame[id].instructions}
-                        materials={cardGame[id].materials}
-                        getCardGame={getCardGame} />)}
-            </div>
-        </div>
+       
     )
 }
 export default Games;
